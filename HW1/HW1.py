8786193cf002a626b8964ef59ddcb257c4ce5615
Shinyships <xxandynguyenxx@gmail.com>
2020-01-28 11:41:51 -0500

    HW1 commit

---
+++ HW1/HW1.py
@@ -0,0 +1,54 @@
+import unittest
+
+def classifyTriangle(a,b,c):
+    if a + b > c and b + c > a and a + c > b and min(a,b,c) > 0:
+        if a == b == c :
+            return "Equilateral Triangle"
+        elif a == b or b == c or a == c:
+            return "Isoceles Triangle"
+        else:
+            if max(a,b,c)**2 == ((a + b + c) - max(a,b,c) - min(a,b,c)) ** 2 + min(a,b,c) ** 2:
+                return 'Right Triangle'
+            else:
+                return "Scalene Triangle"  
+    else:
+        return 'Not A Triangle'
+
+def printClassifyTriangle(a,b,c):
+    print('classifyTriangle(',a, ',', b, ',', c, ')= ' + classifyTriangle(a,b,c))
+
+class TestTriangles(unittest.TestCase):
+    def testNotATriangle(self):
+        self.assertEqual(classifyTriangle(3,3,7), 'Not A Triangle')
+        self.assertEqual(classifyTriangle(3,0,7), 'Not A Triangle')
+        self.assertNotEqual(classifyTriangle(3,3,3), "Not A Triangle")
+
+
+    def testEquilateral(self):  
+        self.assertEqual(classifyTriangle(3,3,3), 'Equilateral Triangle')
+        self.assertNotEqual(classifyTriangle(3,4,3), 'Equilateral Triangle')  
+
+    def testIsoceles(self):
+        self.assertEqual(classifyTriangle(3,5,5), 'Isoceles Triangle')
+        self.assertNotEqual(classifyTriangle(3,4,5), 'Isoceles Triangle')
+
+    def testScalene(self):
+        self.assertEqual(classifyTriangle(3,4,6), 'Scalene Triangle')
+        self.assertNotEqual(classifyTriangle(3,4,5), 'Scalene Triangle')
+        
+    def testRight(self):
+        self.assertEqual(classifyTriangle(3,4,5), 'Right Triangle')
+        self.assertNotEqual(classifyTriangle(6,10,6), 'Right Triangle')
+
+if __name__ == '__main__':
+
+    printClassifyTriangle(6,10,6)
+    printClassifyTriangle(1,3,0)
+    printClassifyTriangle(2,6,6)
+    printClassifyTriangle(6,4,5)
+    printClassifyTriangle(2,2,2)
+    printClassifyTriangle(1,3,5)
+    printClassifyTriangle(1,12,5)
+    printClassifyTriangle(3,4,5)
+
+    unittest.main(exit=True)
